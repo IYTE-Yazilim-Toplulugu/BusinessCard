f8ee6d765bfdc2d944c2d9983a0cd626d28696c6
DogukanTopcu <dogukan_topcu3575@icloud.com>
2023-06-01 19:44:06 +0300

    home page and manager page

---
+++ src/data.js
@@ -0,0 +1,228 @@
+import Image from "./assets/members/katzen.JPG";
+import CoolImage from "./assets/Mask group.png";
+
+export const members = [
+    {
+        url: "dogukantopcu",
+        name: "Doğukan",
+        surname: "TOPÇU",
+        title: {
+            tr: "Yönetim Kurulu Başkanı",
+            en: "Head of Society Board"
+        },
+        social: {
+            insta: "",
+            linkedin: "",
+            github: "",
+        },
+        school: {
+            tr: "İzmir Yüksek Teknoloji Enstitüsü",
+            en: "İzmir Institute of Technology"
+        },
+        department: {
+            tr: "Bilgisayar Mühendisliği",
+            en: "Computer Engineering"
+        },
+        grade: {
+            tr: "1. Sınıf",
+            en: "1. Grade"
+        },
+        email: ["dogukantopcu35@gmail.com"],
+        phone: "+90 553 198 35 75",
+
+        image: Image,
+        coolImage: CoolImage
+    },
+
+    {
+        url: "oguzhanyildirim",
+        name: "Oğzuhan",
+        surname: "YILDIRIM",
+        title: {
+            tr: "Yönetim Kurulu Başkan Yardımcısı",
+            en: "Vice Head of Society Board"
+        },
+        social: {
+            insta: "",
+            linkedin: "",
+            github: "",
+        },
+        school: {
+            tr: "İzmir Yüksek Teknoloji Enstitüsü",
+            en: "İzmir Institute of Technology"
+        },
+        department: {
+            tr: "Bilgisayar Mühendisliği",
+            en: "Computer Engineering"
+        },
+        grade: {
+            tr: "1. Sınıf",
+            en: "1. Grade"
+        },
+        email: ["dogukantopcu35@gmail.com"],
+        phone: "+90 553 198 35 75",
+
+        image: Image,
+        coolImage: CoolImage
+    },
+
+    {
+        url: "tugceturkmenler",
+        name: "Tuğçe",
+        surname: "TÜRKMENLER",
+        title: {
+            tr: "Yönetim Kurulu Tasarım Başkanı",
+            en: "Head of Society Board of Design"
+        },
+        social: {
+            insta: "",
+            linkedin: "",
+            github: "",
+        },
+        school: {
+            tr: "İzmir Yüksek Teknoloji Enstitüsü",
+            en: "İzmir Institute of Technology"
+        },
+        department: {
+            tr: "Bilgisayar Mühendisliği",
+            en: "Computer Engineering"
+        },
+        grade: {
+            tr: "3. Sınıf",
+            en: "3. Grade"
+        },
+        email: ["dogukantopcu35@gmail.com"],
+        phone: "+90 553 198 35 75",
+
+        image: Image,
+        coolImage: CoolImage
+    },
+
+    {
+        url: "akinakdogan",
+        name: "Akın",
+        surname: "AKDOĞAN",
+        title: {
+            tr: "Yönetim Kurulu Organizasyon Başkanı",
+            en: "Head of Society Board of Organization"
+        },
+        social: {
+            insta: "",
+            linkedin: "",
+            github: "",
+        },
+        school: {
+            tr: "İzmir Yüksek Teknoloji Enstitüsü",
+            en: "İzmir Institute of Technology"
+        },
+        department: {
+            tr: "Bilgisayar Mühendisliği",
+            en: "Computer Engineering"
+        },
+        grade: {
+            tr: "1. Sınıf",
+            en: "1. Grade"
+        },
+        email: ["dogukantopcu35@gmail.com"],
+        phone: "+90 553 198 35 75",
+
+        image: Image,
+        coolImage: CoolImage
+    },
+
+    {
+        url: "yigitsariyar",
+        name: "Yiğit",
+        surname: "SARIYAR",
+        title: {
+            tr: "Yönetim Kurulu Organizasyon Başkanı",
+            en: "Head of Society Board of Organization"
+        },
+        social: {
+            insta: "",
+            linkedin: "",
+            github: "",
+        },
+        school: {
+            tr: "İzmir Yüksek Teknoloji Enstitüsü",
+            en: "İzmir Institute of Technology"
+        },
+        department: {
+            tr: "Bilgisayar Mühendisliği",
+            en: "Computer Engineering"
+        },
+        grade: {
+            tr: "1. Sınıf",
+            en: "1. Grade"
+        },
+        email: ["dogukantopcu35@gmail.com"],
+        phone: "+90 553 198 35 75",
+
+        image: Image,
+        coolImage: CoolImage
+    },
+
+    {
+        url: "atakanserce",
+        name: "Atakan",
+        surname: "SERÇE",
+        title: {
+            tr: "Yönetim Kurulu İletişim Başkanı",
+            en: "Head of Society Board of Communication"
+        },
+        social: {
+            insta: "",
+            linkedin: "",
+            github: "",
+        },
+        school: {
+            tr: "İzmir Yüksek Teknoloji Enstitüsü",
+            en: "İzmir Institute of Technology"
+        },
+        department: {
+            tr: "Bilgisayar Mühendisliği",
+            en: "Computer Engineering"
+        },
+        grade: {
+            tr: "1. Sınıf",
+            en: "1. Grade"
+        },
+        email: ["dogukantopcu35@gmail.com"],
+        phone: "+90 553 198 35 75",
+
+        image: Image,
+        coolImage: CoolImage
+    },
+
+    {
+        url: "ahmetkeremsayli",
+        name: "Ahmet Kerem",
+        surname: "ŞAYLI",
+        title: {
+            tr: "Yönetim Kurulu Sosyal Medya Sorumlusu",
+            en: "Head of Society Board of Social Media"
+        },
+        social: {
+            insta: "",
+            linkedin: "",
+            github: "",
+        },
+        school: {
+            tr: "İzmir Yüksek Teknoloji Enstitüsü",
+            en: "İzmir Institute of Technology"
+        },
+        department: {
+            tr: "Bilgisayar Mühendisliği",
+            en: "Computer Engineering"
+        },
+        grade: {
+            tr: "1. Sınıf",
+            en: "1. Grade"
+        },
+        email: ["dogukantopcu35@gmail.com"],
+        phone: "+90 553 198 35 75",
+
+        image: Image,
+        coolImage: CoolImage
+    },
+];
---
+++ src/pages/Home.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+
+import { members } from '../data';
+
+import BasicLogo from "../assets/png-colorful.png";
+import Kedi from "../assets/members/katzen.JPG";
+import Blob from "../assets/members/blob.svg";
+import { useNavigate } from 'react-router-dom';
+
+const Home = () => {
+    const [lang, setLang] = useState(true);
+    const navigation = useNavigate();
+  return (
+    <div className='container mx-auto max-w-md min-h-screen h-full bg-blue-900 text-white pb-10 backGround'>
+
+        <div className='relative top-2 left-0'>
+            {
+                lang ? (
+                    <p className='py-2 cursor-pointer'><span className='font-bold px-2 pl-4 text-green-300' onClick={() => setLang(true)}>Türkçe</span> | <span className='font-bold px-2'  onClick={() => setLang(false)}>English</span></p>
+                ) : (
+                    <p className='py-2 cursor-pointer'><span className='font-bold px-2 pl-4' onClick={() => setLang(true)}>Türkçe</span> | <span className='font-bold px-2 text-green-300'  onClick={() => setLang(false)}>English</span></p>
+                )
+            }
+            
+        </div>
+
+        <div className='flex justify-center items-center pt-20'>
+            <img src={BasicLogo} alt="Iztech Software Society Logo" className='h-48' />
+        </div>
+
+        {
+            lang ? (
+                <h1 className='text-center mt-16 font-extrabold text-2xl'>Yönetim Kurulu Üyeleri</h1>
+            ) : (
+                <h1 className='text-center mt-16 font-extrabold text-2xl'>Society Board Members</h1>
+            )
+        }
+
+
+        <div className='grid grid-cols-2 px-4 my-12'>
+
+            {
+                members.map((member, i) => (
+                    <div className='cursor-pointer' key={i} onClick={() => lang ? navigation(`/tr/${member.url}`) : navigation(`/en/${member.url}`) }>
+                        <div className='flex justify-center items-center'>
+                            <img src={Blob} alt="bg" className='absolute z-0 lg:hidden w-[50vw] -mt-16' />
+                            <img className="mask z-10" src={member.image} alt="member" />
+                        </div>
+                        <div className='flex flex-col justify-center items-center -mt-16 z-20'>
+                            <h2 className='font-bold text-lg text-center'>{member.name + " " + member.surname}</h2>
+                            <p className='font-light text-sm -mt-1.5 text-center'>{lang ? member.title.tr : member.title.en}</p>
+                        </div>
+                    </div>
+                ))
+            }
+            
+        </div>
+    </div>
+  )
+}
+
+export default Home
